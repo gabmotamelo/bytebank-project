c8478d2a788854806a5a837e6bb9f080b64a5cf1
Gabriel Mota <gabrielmota6@hotmail.com>
2022-03-23 15:53:10 -0300

    Adicionando 'abstract' em algumas classes

--- bytebank-herdado/src/Designer.java
+++ bytebank-herdado/src/Designer.java
@@ -1,9 +1,7 @@
 public class Designer extends Funcionario {
 
-    public double getBonificacao(){
+    public double getBonificacao() {
         System.out.println("Chamando o método de bonificação do DESIGNER: ");
         return 200;
     }
-
-
 }
